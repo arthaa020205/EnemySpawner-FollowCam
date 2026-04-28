20295278ffa773f5c3c5c851937e9785bb9b2649
Arthann_e05 <arthanugraha020205@gmail.com>
2026-04-28 13:51:47 +0700

    Add files via upload

---
+++ Script/EnemySpawner.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    [SerializeField]
+    private float _MinimumSpawnTime;
+
+    [SerializeField]
+    private float _MaximumSpawnTime;
+
+    private float _timeUntilSpawn;
+
+    void Awake()
+    { 
+        SetTimeUntilSpawn();
+    }
+
+    void Update()
+    {
+        _timeUntilSpawn -= Time.deltaTime;
+
+        if (_timeUntilSpawn <= 0)
+        {
+            Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+    }
+
+    private void SetTimeUntilSpawn()
+    {
+        _timeUntilSpawn = Random.Range(_MinimumSpawnTime, _MaximumSpawnTime);
+    }
+}
